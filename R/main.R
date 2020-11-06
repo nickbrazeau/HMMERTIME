76cb8f89bc96888665892c1237c63f52111b9323
nickbrazeau <nbrazeau@med.unc.edu>
2020-11-05 20:11:39 -0500

    tidy up vign and docs

--- R/main.R
+++ R/main.R
@@ -1,141 +1,10 @@
-#------------------------------------------------
-#' Run polyIBD MCMC using Rcpp functions
-#'
-#' @param input TODO
-#' @param m_max TODO
-#' @param k_max TODO
-#' @param rho TODO
-#' @param burnin TODO
-#' @param samples TODO
-#' @param e1 TODO
-#' @param e2 TODO
-#' @param reportIteration TODO
-#'
-#' @description words todo
-#'
-#' @return words todo
-#'
-#' @export
-
-
-runMCMC <- function(input = NULL,
-                    m_max = 5,
-                    k_max = 10,
-                    rho = 1e-5,
-                    burnin = 1e2,
-                    samples = 1e3,
-                    e1 = 0.05, e2 = 0.05,
-                    reportIteration = 1e3) {
-
-
-  # TODO - input parameter checks
-  # note - vcf must have 4 columns, samples in final two columns
-  # better management of memory for p and vcf -- if you have it as one object, p continually gets copied and takes up way too much memory
-  # assertions
-
-
-
-  # ------------------------------
-  #     Setup Input for Rcpp
-  # ------------------------------
-
-  Rcppcompat <- polyIBDinput_to_Rcppcompat(input)
-
-  # define list of arguments to pass to Rcpp
-  args <- list(x = Rcppcompat[["x"]],
-               p = unlist(Rcppcompat[["p"]]),
-               rho = rho,
-               SNP_dist = Rcppcompat[["SNP_dist"]],
-               m_max = m_max,
-               k_max = k_max,
-               burnin = burnin,
-               samples = samples,
-               e1 = e1,
-               e2 = e2,
-               reportIteration = reportIteration)
-
-  # R functions to pass to Rcpp
-  args_functions <- list(getTransProbs = polyIBD::getTransProbs)
-
-
-  # ------------------------------
-  #            RUN MCMC
-  # ------------------------------
-
-  # run efficient Rcpp function
-  output_raw <- runMCMC_cpp(args, args_functions)
-
-  # check for convergence
-  checkConvergence(output_raw$logLike_burnin, output_raw$logLike)
-
-
-  # ------------------------------
-  #      SAVE MCMC RAW OUTPUT
-  # ------------------------------
-
-  # list of raw output
-  raw_output <- list(logLike_burnin = coda::mcmc(output_raw$logLike_burnin),
-                     logLike = coda::mcmc(output_raw$logLike),
-                     m1 = coda::mcmc(output_raw$m1),
-                     m2 = coda::mcmc(output_raw$m2),
-                     f = coda::mcmc(output_raw$f),
-                     f_ind = coda::mcmc(output_raw$f_ind),
-                     k = coda::mcmc(output_raw$k)
-                     )
-
-
-  # ------------------------------
-  #   SAVE MCMC SUMMARY OUTPUT
-  # ------------------------------
-
-  # get marginal IBD matrix
-  IBD_marginal <- t(Rcpp_to_mat(output_raw$IBD_marginal))
-  colnames(IBD_marginal) <- paste0("z", 0:(ncol(IBD_marginal)-1))
-  IBD_marginal <- cbind(input$CHROMPOS, IBD_marginal)
-
-  # get final acceptance rate
-  accept_rate <- output_raw$accept_rate/samples
-
-  # get time it took MCMC to run
-  runTime <- output_raw$runTime
-
-  # calculate quantiles over parameters
-  quants <- t(mapply(function(x){quantile(x, probs=c(0.025, 0.5, 0.975))}, raw_output))
-  quants <- quants[rownames(quants) %in% c("m1", "m2", "f", "f_ind", "k"),]
-
-  # list of summary output
-  summary_output <- list(IBD_marginal = IBD_marginal,
-                         quantiles = quants,
-                         accept_rate = accept_rate,
-                         runTime = runTime)
-
-
-  # ------------------------------
-  #            RETURN
-  # ------------------------------
-  # create output class polyIBD
-  ret <- list(samples = colnames(input$gtmatrix),
-              summary = summary_output,
-              iterations = raw_output)
-  class(ret) <- "polyIBD"
-
-  # return
-  return(ret)
-}
-
-
-#------------------------------------------------
-# Trans Probs
-#------------------------------------------------
-# ------------------------------------------------------------------
 #' @title Get transition probabilities
-#'
 #' @description Takes values of f, rho, and zmax. Produces rate matrix and calculates eigen values and vectors.
 #' @param f TODO
 #' @param rho TODO
 #' @param k TODO
 #' @param z_max TODO
-#' @export
+#' @noRd
 
 getTransProbs <- function(f, rho, k, z_max) {
 
@@ -162,8 +31,269 @@ getTransProbs <- function(f, rho, k, z_max) {
 
 
 
+#' @title mat_to_Rcpp
+#' @description  takes matrix as input, converts to list format for use within Rcpp code. Needed for trans prob eigen method
+#' @noRd
 
-# -----------------------------------
+mat_to_Rcpp <- function(x) {
+  return(split(x,f=1:nrow(x)))
+}
+
+#' @title Rcpp_to_mat
+#' @description Takes list format returned from Rcpp and converts to matrix. Needed for trans prob eigen method
+#' @noRd
+
+Rcpp_to_mat <- function(x) {
+  ret <- matrix(unlist(x), nrow=length(x), byrow=TRUE)
+  return(ret)
+}
+
+#' @title Pairwise Genetic Combinations
+#' @description Get genotype pairwise combinations for Cpp
+#' @noRd
+
+pair_gen_combns <- function(pairmatrix) {
+  # compare two samples and save comparison type in vector x
+  # x is an integer vector with values in 0:15. These values indicate genotype combinations that cycle through the four options: {missing, homo REF, het, homo ALT} in the first sample, then the same four options in the second sample, leading to 16 options in total
+  # 0 = {NA, NA}
+  # 1 = {NA, A}
+  # 2 = {NA, Aa}
+  # 3 = {NA, a}
+  # 4 = {A, NA}
+  # 5 = {A,A}
+  # 6 = {A,Aa}
+  # 7 = {A,a}
+  # 8 = {Aa, NA}
+  # 9 = {Aa,A}
+  # 10 = {Aa,Aa}
+  # 11 = {Aa,a}
+  # 12 = {a, NA}
+  # 13 = {a,A}
+  # 14 = {a,Aa}
+  # 15 = {a,a}
+
+  ret <- 4*(pairmatrix[,1]+1) + (pairmatrix[,2]+1)
+  return(ret)
+}
+
+
+#' Run polyIBD MCMC using Rcpp functions
+#'
+#' @param input TODO
+#' @param m_max TODO
+#' @param k_max TODO
+#' @param rho TODO
+#' @param burnin TODO
+#' @param samples TODO
+#' @param e1 TODO
+#' @param e2 TODO
+#' @param reportIteration TODO
+#' @description words todo
+#' @return words todo
+#' @importFrom magrittr %>%
+#' @export
+
+
+runMCMC <- function(vcfRobj = NULL,
+                    vcfploid = 2,
+                    m_max = 5,
+                    k_max = 10,
+                    rho = 1e-5,
+                    e1 = 0.05,
+                    e2 = 0.05,
+                    burnin = 1e2,
+                    samples = 1e3,
+                    verbose = TRUE,
+                    reportIteration = 1e3,
+                    parallelize = TRUE) {
+
+  #............................................................
+  # Assertions
+  #...........................................................
+  #......................
+  # assertions and checks on vcf
+  #......................
+  # check if input is vcfR
+  if(! class(vcfRobj) %in% "vcfR" ){
+    stop("vcfR object must be of class vcfR")
+  }
+  # check if vcf is biallelic snps
+  if( ! identical(vcfRobj, vcfR::extract.indels(vcfRobj[vcfR::is.biallelic(vcfRobj)], return.indels = F) ) ){
+    stop("Your vcf input contains variants that are not biallelic SNPs. HMMERTIME
+          is restricted to use cases with biallelic SNPs. To convert your vcfR
+          object to biallelic SNPs, you can use the following command:
+          vcfR::extract.indels(vcfRobj[vcfR::is.biallelic(vcfRobj)], return.indels = F)
+         ")
+  }
+  # check ploidy
+  assert_in(vcfploid, c(1, 2),
+            message = "VCF ploidy %s must be in either 1 or 2")
+
+  ploidy <- as.numeric(sub("ploidy=", "", stringr::str_extract(vcfRobj@meta[grepl("ploidy", vcfRobj@meta)], "ploidy=[0-9]")))
+  assert_eq(vcfploid, ploidy,
+            message = "You have indcated that your VCF ploidy is %s but the
+                        ploidy in the VCF metadata is %s")
+  #......................
+  # assertions on input parameters
+  #......................
+  assert_single_pos_int(m_max)
+  assert_single_pos_int(k_max)
+  assert_single_bounded(rho) # recombination rate bounded by 0 and 1
+  assert_single_bounded(e1)
+  assert_single_bounded(e2)
+  assert_single_pos_int(burnin)
+  assert_single_pos_int(samples)
+  assert_single_pos_int(reportIteration)
+
+
+
+  #............................................................
+  # Wrangle data for Cpp input
+  #...........................................................
+  #......................
+  # pieces for input
+  #......................
+  gtmatrix <- vcfRmanip::gtmat012(vcfRobj)
+  # drop any rownames
+  rownames(gtmatrix) <- NULL
+  CHROM <- vcfR::getCHROM(vcfRobj)
+  POS <- vcfR::getPOS(vcfRobj)
+  # population loci-specific allele freq
+  L <- length(POS)
+  p <- rep(NA, length(POS))
+  p <- apply(gtmatrix, 1,
+             function(x){(2*(length(which(x==0))) + length(which(x==1)))/(2*length(x))}) # since we know homozygous ref is 0, so this counts as 2 As, and then we count hets and then divide by 2*possible alleles. Doing this roundabout way because we aren't in HWE
+  # get distances between snps
+  SNP_dist <- diff(POS)
+  # find "jumps" between chroms
+  chromtab <- table(CHROM)
+  nc <- length(chromtab)
+  n <- as.vector(chromtab)
+  SNP_dist[cumsum(n)[1:(nc-1)]] <- -1
+
+  #......................
+  # check if pairwise extensions need to be made
+  #......................
+  # does vcf contain more than two samples
+  if (ncol(gtmatrix) > 2) {
+
+    # get combinations
+    gtcombs <- combn(colnames(gtmatrix), 2)
+    gtcombs_list <- split(gtcombs, 1:ncol(gtcombs))
+    # split up pairwise
+    pairmatrix_list <- lapply(gtcombs_list, function(x){gtmatrix[, x]})
+    # liftover for genotypes
+    pairmatrix_list <- lapply(pairmatrix_list, HMMERTIME:::pair_gen_combns)
+
+  } else {
+
+    pairmatrix_list <- HMMERTIME:::pair_gen_combns(gtmatrix)
+
+  }
+
+  # tidy this up for output
+  tidyout <- tibble::tibble(smpl1 = gtcombs[1,],
+                            smpl2 = gtcombs[2,]) %>%
+    dplyr::mutate(pairmat = pairmatrix_list)
+
+  # check
+  assert_eq(length(pairmatrix_list), choose(ncol(gtmatrix), 2),
+            message = "Issue with splitting your VCF. Check if your VCF
+                       has duplicate sample names")
+
+  #............................................................
+  # print out message about pairwise user is about to do
+  #...........................................................
+  if (verbose) {
+    cat(crayon::cyan("*~*~*~*~ HMMERTIME Run Summary ~*~*~*~*\n"))
+    cat(crayon::green("Samples in VCF: "), ncol(gtmatrix), "\n")
+    cat(crayon::magenta("Pairwise Comparisons: "), length(pairmatrix_list), "\n")
+    cat(crayon::blue("Burn-in for Each: "), burnin, "\n")
+    cat(crayon::blue("Sampling for Each: "), samples, "\n")
+  }
+
+  #............................................................
+  # run efficient Rcpp function
+  #...........................................................
+  # internal wrapper
+  my_MCMC_wrapper <- function(pairmat) {
+
+    # define list of "set" parameters to pass to Rcpp
+    args <- list(x = pairmat,
+                 p = p,
+                 rho = rho,
+                 SNP_dist = SNP_dist,
+                 m_max = m_max,
+                 k_max = k_max,
+                 burnin = burnin,
+                 samples = samples,
+                 e1 = e1,
+                 e2 = e2,
+                 reportIteration = reportIteration)
+
+
+    # R functions to pass to Rcpp
+    args_functions <- list(getTransProbs = HMMERTIME:::getTransProbs)
+
+    # MCMC
+    output_raw <- runMCMC_cpp(args, args_functions)
+
+    # check for convergence
+    checkConvergence(output_raw$logLike_burnin, output_raw$logLike)
+
+    # list of raw output
+    raw_output <- list(logLike_burnin = coda::mcmc(output_raw$logLike_burnin),
+                       logLike = coda::mcmc(output_raw$logLike),
+                       m1 = coda::mcmc(output_raw$m1),
+                       m2 = coda::mcmc(output_raw$m2),
+                       f = coda::mcmc(output_raw$f),
+                       k = coda::mcmc(output_raw$k))
+
+    # get marginal IBD matrix
+    IBD_marginal <- t(Rcpp_to_mat(output_raw$IBD_marginal))
+    colnames(IBD_marginal) <- paste0("z", 0:(ncol(IBD_marginal)-1))
+    # tidy up
+    IBD_marginal <- IBD_marginal %>%
+      dplyr::mutate(CHROM = CHROM,
+                    POS = POS) %>%
+      dplyr::select(c("CHROM", "POS", dplyr::everything()))
+
+    # get final acceptance rate
+    accept_rate <- output_raw$accept_rate/samples
+
+    # get time it took MCMC to run
+    runTime <- output_raw$runTime
+
+    # calculate quantiles over parameters
+    quants <- t(mapply(function(x){quantile(x, probs=c(0.025, 0.5, 0.975))}, raw_output))
+    quants <- quants[rownames(quants) %in% c("m1", "m2", "f", "k"),]
+
+    # list of summary output
+    summary_output <- list(IBD_marginal = IBD_marginal,
+                           quantiles = quants,
+                           accept_rate = accept_rate,
+                           runTime = runTime)
+
+    # create output
+    ret <- list(summary = summary_output,
+                iterations = raw_output)
+    # return
+    return(ret)
+  }
+
+  # wrapper over pairwise
+  if (!parallelize) {
+    tidyout <- tidyout %>%
+      dplyr::mutate(mcmcout = furrr::future_map(pairmat, my_MCMC_wrapper))
+  }
+
+
+}
+
+
+
+
+
 # checkConvergence
 # calculates Geweke statistic from a series of burn-in and sampling draws. Report whether burn-in length was sufficient based on this statistic.
 # (not exported)
--- R/utils.R
+++ R/utils.R
@@ -1,149 +1,4 @@
-#' @title Convert a \code{vcfRobject} to ----TEMP -- polyIBDinput
-#' @description This function wraps the \code{vcfR} package to convert a vcffile or \code{vcfR} object to a SNP matrix suitable for polyIBD.
-#' @param vcffile is the file path that for the VCF. This VCF will be converted to an object of class \code{vcfR}.
-#' @param vcfR is the file path that for the VCF. This VCF will be converted to an object of class \code{vcfR}.
-#'
-#' @export
-
-
-# TODO:
-#     Change these functions to one call as an S4 object with slots for gtmatrix, GT, PopAf, and d
-#     better class definitions
-#     snpmatrixlist needs to be in an apply loop not for loop
-#     better management of memory with p
-
-vcf2HMMERTIMEinput <- function(vcffile=NULL, vcfR=NULL) {
-
-
-  # check if input is vcfR
-  if(is.null(vcffile)){
-    if(! class(vcfR) %in% "vcfR" ){
-      stop("vcfR object must be of class vcfR")
-    }
-    # consistent naming
-    vcf <- vcfR
-
-    # check if input is vcf file path
-  } else if (!is.null(vcffile)){
-    vcf <- vcfR::read.vcfR(file=vcffile, verbose=F) # read vcf
-  } else {
-    stop("Must specify an input")
-  }
-
-  if( ! identical( vcf, vcfR::extract.indels(vcf[vcfR::is.biallelic(vcf)], return.indels = F) ) ){
-    stop("Your vcf input contains variants that are not biallelic SNPs. polyIBD
-          is restricted to use cases with biallelic SNPs. To convert your vcfR
-          object to biallelic SNPs, you can use the following command:
-          vcfR::extract.indels(vcf[vcfR::is.biallelic(vcf)], return.indels = F)
-         ")
-  }
-
-
-  # -----------------------------------------------------
-  # determine ploidy to determine genotype numeric placeholder
-  #------------------------------------------------------
-  ploidy <- stringr::str_extract(vcf@meta[grepl("ploidy", vcf@meta)], "ploidy=[0-9]")
-  ploidy <- stringr::str_split(t(ploidy), "=", simplify = T)
-  ploidy <- as.numeric(ploidy[1,2])
-
-  if(ploidy == 2){
-
-    gtmatrix <- vcfR::extract.gt(vcf, element='GT', as.numeric=F) # numeric as T doesn't parse 0/1 correctly
-    gtmatrix[gtmatrix == "0/0"] <- 0
-    gtmatrix[gtmatrix == "0/1"] <- 1
-    gtmatrix[gtmatrix == "1/1"] <- 2
-    gtmatrix[is.na(gtmatrix)] <- -1
-    gtmatrix <- apply(gtmatrix, 2, function(x){as.numeric(x)}) # need to convert from char (--dependent on case of "/") to numeric
-  } else if(ploidy == 1){
-    gtmatrix <- vcfR::extract.gt(vcf, element='GT', as.numeric=T)
-    gtmatrix[gtmatrix == 0] <- 0
-    gtmatrix[gtmatrix == 1] <- 2
-    gtmatrix[is.na(gtmatrix)] <- -1
-  } else {
-    stop("You have a ploidy that is less than 1 or greater than 3, which cannot be accomodated by polyIBD")
-  }
-  # -----------------------------------------------------
-  # Store CHROM and POS from the VCF
-  #------------------------------------------------------
-  CHROM <- vcfR::getCHROM(vcf)
-  POS <- vcfR::getPOS(vcf)
-
-  # -----------------------------------------------------
-  # Calculate pop AF, p
-  #------------------------------------------------------
-  L <- length(POS)
-  p <- rep(NA, length(POS))
-  p <- apply(gtmatrix, 1,
-             function(x){(2*(length(which(x==0))) + length(which(x==1)))/(2*length(x))}) # since we know homozygous ref is 0, so this counts as 2 As, and then we count hets and then divide by 2*possible alleles. Doing this roundabout way because we aren't in HWE
-
-
-  # -----------------------------------------------------
-  # return
-  #-----------------------------------------------------
-  retlist <- list(CHROMPOS = cbind.data.frame(CHROM, POS),
-                  gtmatrix = gtmatrix,
-                  p=p)
-  class(retlist) <- "polyIBDinput"
-  return(retlist)
-
-}
-
-
-
-# -----------------------------------
-# HMMERTIMEinput_to_Rcppcompat
-# Takes the polyIBDinput and makes it easier to be parsed for the Rcpp args.
-# (not exported)
-
-HMMERTIMEinput_to_Rcppcompat <- function(polyIBDinput){
-
-  p <- polyIBDinput[["p"]]
-  gtmatrix <- polyIBDinput[["gtmatrix"]]
-
-  # extract basic parameters
-  CHROMtab <- table(polyIBDinput$CHROMPOS[, 1])
-  nc <- length(CHROMtab)
-  cnames <- names(CHROMtab)
-  n <- as.vector(CHROMtab)
-
-  # get distances between SNPs. Distance=-1 between contigs, indicating infinite distance
-  SNP_dist <- diff(polyIBDinput$CHROMPOS[, 2]) # second column in this class is POS
-  SNP_dist[cumsum(n)[1:(nc-1)]] <- -1
-
-  # compare two samples and save comparison type in vector x
-  # x is an integer vector with values in 0:15. These values indicate genotype combinations that cycle through the four options: {missing, homo REF, het, homo ALT} in the first sample, then the same four options in the second sample, leading to 16 options in total
-  # 0 = {NA, NA}
-  # 1 = {NA, A}
-  # 2 = {NA, Aa}
-  # 3 = {NA, a}
-  # 4 = {A, NA}
-  # 5 = {A,A}
-  # 6 = {A,Aa}
-  # 7 = {A,a}
-  # 8 = {Aa, NA}
-  # 9 = {Aa,A}
-  # 10 = {Aa,Aa}
-  # 11 = {Aa,a}
-  # 12 = {a, NA}
-  # 13 = {a,A}
-  # 14 = {a,Aa}
-  # 15 = {a,a}
-
-  x <- 4*(gtmatrix[,1]+1) + (gtmatrix[,2]+1)
-
-  ## return
-  ret <- list(p=p,
-              x=x,
-              SNP_dist = SNP_dist)
-
-  return(ret)
-
-}
-
-
-
-#------------------------------------------------
-#' @description function for determining if object is of class polyIBD
+#' @description function for determining if object is of class HMMERTIME
 #' @noRd
 #' @export
 
@@ -151,7 +6,6 @@ is.HMMERTIME <- function(x) {
   inherits(x, "is.HMMERTIME")
 }
 
-#------------------------------------------------
 #' @description overload print() function to print summary only
 #' @noRd
 #' @export
@@ -164,9 +18,8 @@ print.HMMERTIME <- function(x, ...) {
   invisible(x)
 }
 
-#------------------------------------------------
 #' @description overload summary() function.
-#' @noRD
+#' @noRd
 #' @export
 summary.HMMERTIME <- function(x, ...) {
 
@@ -184,9 +37,8 @@ summary.HMMERTIME <- function(x, ...) {
   print(quants)
 }
 
-#------------------------------------------------
-#' @description function for determining if object is of class polyIBD
-#' @noRD
+#' @description function for determining if object is of class HMMERTIME
+#' @noRd
 #' @export
 
 is.HMMERTIMEinput <- function(x) {
@@ -194,9 +46,8 @@ is.HMMERTIMEinput <- function(x) {
 }
 
 
-#------------------------------------------------
 #' @description overload print() function to print summary only
-#' @noRD
+#' @noRd
 #' @export
 
 print.HMMERTIMEinput <- function(x, ...) {
@@ -210,10 +61,8 @@ print.HMMERTIMEinput <- function(x, ...) {
   invisible(x)
 }
 
-
-#------------------------------------------------
 #' @description overload summary() function to print summary only
-#' @noRD
+#' @noRd
 #' @export
 summary.HMMERTIMEinput <- function(x, ...) {
 
@@ -227,27 +76,3 @@ summary.HMMERTIMEinput <- function(x, ...) {
 }
 
 
-
-
-#------------------------------------------------
-# R <> Cpp compatibility for trans probs
-#------------------------------------------------
-
-# -----------------------------------
-#' @title mat_to_Rcpp
-#' @description  takes matrix as input, converts to list format for use within Rcpp code. Needed for trans prob eigen method
-#' @noRd
-
-mat_to_Rcpp <- function(x) {
-  return(split(x,f=1:nrow(x)))
-}
-
-# -----------------------------------
-#' @title Rcpp_to_mat
-#' @description Takes list format returned from Rcpp and converts to matrix. Needed for trans prob eigen method
-#' @noRd
-
-Rcpp_to_mat <- function(x) {
-  ret <- matrix(unlist(x), nrow=length(x), byrow=TRUE)
-  return(ret)
-}
